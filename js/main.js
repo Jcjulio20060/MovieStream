68d6d5fb73b8acdcc8aaca913661607546c3ce13
Júlio <73503140+Jcjulio20060@users.noreply.github.com>
2024-12-26 15:46:23 -0300

    Alteração final do slider

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 $(document).ready(function(){
     $('.slider-container').slick({
-      slidesToShow: 8, /* Número de cards visíveis por vez */
-      slidesToScroll: 1, /* Número de cards a rolar por clique */
-      arrows: true, /* Habilita botões de navegação */
-      prevArrow: $('.prev-btn'), /* Elemento do botão anterior */
-      nextArrow: $('.next-btn'), /* Elemento do botão seguinte */
+      slidesToShow: 8,
+      slidesToScroll: 1,
+      infinite: false,
+      arrows: true,
+      prevArrow: $('.prev-btn'),
+      nextArrow: $('.next-btn')
     });
-});
+  }).on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+    if (nextSlide === 0) {
+      $('.prev-btn').addClass('disabled');
+    } else {
+      $('.prev-btn').removeClass('disabled');
+    }
+    if (nextSlide === slick.slideCount - 1) {
+      $('.next-btn').addClass('disabled');
+    } else {
+      $('.next-btn').removeClass('disabled');
+    }
+  });
